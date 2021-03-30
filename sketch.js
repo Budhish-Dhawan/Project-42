cee469c39923f08e5d7ef8f03d9f79919fcfe430
Budhish-Dhawan <budhishdhawan@gmail.com>
2021-03-30 10:13:05 +0530

    monkey go happy game

--- sketch.js
+++ sketch.js
@@ -9,7 +9,9 @@ var gameState = PLAY;
 function preload(){
   backImage=loadImage("jungle.jpg");
   player_running = loadAnimation("Monkey_01.png","Monkey_02.png","Monkey_03.png","Monkey_04.png","Monkey_05.png","Monkey_06.png","Monkey_07.png","Monkey_08.png","Monkey_09.png","Monkey_10.png");
-
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("stone.png");
+  gameOverImage = loadImage("gameOver.png");
 }
 
 function setup() {
@@ -28,7 +30,10 @@ function setup() {
   ground = createSprite(400,350,800,10);
   ground.x=ground.width/2;
   ground.visible=false;
+
   
+  FoodGroup = createGroup();
+  ObstaclesGroup = createGroup();
 }
 
 function draw() { 
@@ -47,7 +52,44 @@ function draw() {
   
     player.collide(ground);
 
-  }
+    if(FoodGroup.isTouching(player)){
+      FoodGroup.destroyEach();
+      //score = score + 2;
+      player.scale = player.scale + 0.05;
+      score = score+1;
+    }
 
+    spawnFood();
+    spawnObstacle();
+    
+  }
   drawSprites();
+}
+
+function spawnFood(){
+  if(frameCount%80 === 0){
+    var banana = createSprite(600, 250, 40, 10);
+    banana.y = random(150, 200);
+    banana.addImage(bananaImage);
+    banana.scale = 0.05;
+    banana.velocityX = -4;
+
+    banana.lifetime = 300;
+    player.depth = banana.depth+1;
+    FoodGroup.add(banana);
+  }
+}
+
+function spawnObstacle(){
+  if(frameCount%120 === 0){
+    var obstacle = createSprite(600, 250, 40, 10);
+    obstacle.y = random(140, 200);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.05;
+    obstacle.velocityX = -4;
+
+    obstacle.lifetime = 300;
+    player.depth = obstacle.depth + 1;
+    ObstaclesGroup.add(obstacle);
+  }
 }
